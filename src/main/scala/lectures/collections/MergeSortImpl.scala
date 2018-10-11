218056b029c3667bd7f092345b71513bb9235e73
Anastasia Tomilina <a.i.tomilina@tinkoff.ru>
2018-10-11 22:55:14 +0300

    [hw4] MergeSort

--- src/main/scala/lectures/collections/MergeSortImpl.scala
+++ src/main/scala/lectures/collections/MergeSortImpl.scala
@@ -7,6 +7,24 @@ package lectures.collections
   */
 object MergeSortImpl extends App {
 
-  def mergeSort(data: Seq[Int]): Seq[Int] = data
+  def mergeSort(data: Seq[Int]): Seq[Int] = {
+    def merge(left: Seq[Int], right: Seq[Int]): Seq[Int] =
+      (left, right) match {
+        case (Nil, _) => right
+        case (_, Nil) => left
+        case (left_head :: left_tail, right_head :: right_tail) =>
+          if (left_head < right_head) left_head +: merge(left_tail, right)
+          else right_head +: merge(left, right_tail)
+      }
+
+    val n = data.length / 2
+    if (n == 0) data
+    else {
+      val (left, right) = data splitAt n
+      merge(mergeSort(left), mergeSort(right))
+    }
+  }
+
+  print(mergeSort(Seq(3, 4, 2, 5, 1)));
 
 }
